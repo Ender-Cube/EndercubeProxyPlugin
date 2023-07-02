295eafe01bb492aa62568eeb7e57d34c334388ad
zax71 <zabax32@gmail.com>
2023-07-02 16:58:28 +0100

    Remove unused files

--- src/main/templates/me/zax71/endercubeproxyplugin/BuildConstants.java
+++
@@ -1,7 +0,0 @@
-package me.zax71.endercubeproxyplugin;
-
-// The constants are replaced before compilation
-public class BuildConstants {
-
-    public static final String VERSION = "${version}";
-}
